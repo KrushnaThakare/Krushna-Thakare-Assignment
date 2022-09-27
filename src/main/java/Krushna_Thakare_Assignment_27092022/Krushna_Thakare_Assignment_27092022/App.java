3f85f532ed82a89d808f8bdf8e0e6d22a8a8c287
KrushnaThakare <krushna.gopal96@gmail.com>
2022-09-27 16:11:51 +0530

    Added All Dependencies In pom

---
+++ src/main/java/Krushna_Thakare_Assignment_27092022/Krushna_Thakare_Assignment_27092022/App.java
@@ -0,0 +1,7 @@
+package Krushna_Thakare_Assignment_27092022.Krushna_Thakare_Assignment_27092022;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Krushna_Thakare_Assignment_27092022/Krushna_Thakare_Assignment_27092022/AppTest.java
@@ -0,0 +1,13 @@
+package Krushna_Thakare_Assignment_27092022.Krushna_Thakare_Assignment_27092022;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
